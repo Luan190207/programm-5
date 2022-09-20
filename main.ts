af7eba073c7e6d80cd93ff713bac4c01e5a1ee54
Luan190207 <113418915+Luan190207@users.noreply.github.com>
2022-09-20 08:42:29 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
 })
